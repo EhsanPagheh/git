ef14cb99d2dcaafd40f64b9bc0be393d696e9794
Ehsan <ehsanpaghe2000@gmail.com>
2022-04-20 12:24:27 +0430

    add main project

---
+++ src/com/ehsanapp/helloworld/HiWorld.java
@@ -0,0 +1,8 @@
+package com.ehsanapp.helloworld;
+
+public class HiWorld {
+
+  public static void main(String[] args) {
+    System.out.println("hello guys this is my first intellij project");
+  }
+}
